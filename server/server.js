3e9bbd473fa6ce584b15c14ddd1cc3ebd6e8c4b6
Jason Vance <jbvance@gmail.com>
2018-02-20 12:08:47 -0600

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ app.delete('/todos/:id', (req, res) => {
   Todo.findByIdAndRemove(id)
     .then(todo => {
       if (!todo) {
-        return res.status(404).send()
+        return res.status(404).send();
       }
       res.status(200).send(todo);
     })
